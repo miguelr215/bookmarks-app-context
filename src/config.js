2b1506b394383d85db2bc5c9051c0ddd732addcb
drexelramos <drexelramos@gmail.com>
2020-11-22 06:48:47 -0800

    initial commit

---
+++ src/config.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+    API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
+    API_KEY: '$2a$10$FDBbOeg4IS6b57HzJDD5XeQhJNe3SS6uBYaSERfZMrC3QMvjuHZP2',
+  }
+  
